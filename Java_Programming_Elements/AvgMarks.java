882d3411aa23c23103b1bd325ba60f50be12ef3d
sbmaurya <sbmaurya066@gmail.com>
2025-06-19 10:03:59 +0530

    [Shubham Maurya] Add. Practice Problems Soln!

---
+++ Java_Programming_Elements/AvgMarks.java
@@ -0,0 +1,10 @@
+public class AvgMarks {
+    public static void main(String[] args) {
+        int Maths = 94;
+        int Physics = 95;
+        int Chemistry = 96;
+
+        int average = (Maths+Physics+Chemistry)/3;
+        System.out.println("Sam’s average mark in PCM is  " + average);
+    }
+}
